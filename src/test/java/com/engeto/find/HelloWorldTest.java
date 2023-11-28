56c0a82ced3db16a4990b6cd7e02bb34e1b3ebd0
Lucie Houdkova <lucachat@seznam.cz>
2023-11-28 14:37:40 +0100

    Ukol z 10 lekce

---
+++ src/test/java/com/engeto/find/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.engeto.find;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+
+    public void printHelloTest() {
+
+        System.out.println("Hello world!");
+
+    }
+
+}
